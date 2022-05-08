c73d91c8f543974b8279b52f951c29cd3d6153a4
kunal <kunal.py3@gmail.com>
2022-05-08 15:12:09 +0530

    Basic Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
+
+
+// ======================================================
+// | One file need to be created from your side.          |
+// | create fb.config.ts 🔥                               |  
+// | export auth, app, fireStore, storage.                |    
+// | For more read this https://firebase.google.com/docs  |
+// ======================================================
